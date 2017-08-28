341db0ef70c4a5fa50ac43d3bd1f786298819515
Ugho STEPHAN <ugho.stephan@consertotech.pro>
2017-08-28 12:01:32 +0200

    WIP end of course 156

---
+++ Assets/Scripts/AttackerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerController : MonoBehaviour {
+
+	[Range(-1f, 1f)]
+	public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D rigidbody2D = gameObject.AddComponent<Rigidbody2D> ();
+		rigidbody2D.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Debug.Log (name + " trigger enter");
+	}
+}
